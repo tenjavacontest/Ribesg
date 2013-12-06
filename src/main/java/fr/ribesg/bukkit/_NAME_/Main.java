f0405ddf869f7dccfb76927512e8484005dfbeb0
Ribesg <ribesg@yahoo.fr>
2013-12-06 13:05:09 +0100

    Add skeleton, fix pom - 1600

---
+++ src/main/java/fr/ribesg/bukkit/_NAME_/Main.java
@@ -0,0 +1,26 @@
+/***************************************************************************
+ * Project file:    Ten.Java#2 - _NAME_ - Main.java                        *
+ * Full Class name: fr.ribesg.bukkit._NAME_.Main                           *
+ *                                                                         *
+ *                Copyright (c) 2013 Ribesg - www.ribesg.fr                *
+ *   This file is under GPLv3 -> http://www.gnu.org/licenses/gpl-3.0.txt   *
+ *    Please contact me at ribesg[at]yahoo.fr if you improve this file!    *
+ ***************************************************************************/
+
+package fr.ribesg.bukkit._NAME_;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		// TODO
+	}
+
+	@Override
+	public void onDisable() {
+		// TODO
+	}
+
+}
